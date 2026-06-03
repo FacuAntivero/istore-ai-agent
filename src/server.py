95712a3b34d177381b71c341256ccb29b9afd79b
Facundo Antivero <73594978+FacuAntivero@users.noreply.github.com>
2026-06-03 20:04:37 -0300

    arreglando errores

--- src/server.py
+++ src/server.py
@@ -25,9 +25,8 @@ app = FastAPI(title="iStore AI Webhook")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-    "http://localhost:3000",
-    "https://istore-admin.vercel.app", 
-    "*" # Dejá el asterisco hoy así podemos probar en paz
+        "http://localhost:3000",
+        "https://istore-admin.vercel.app" 
     ],
     allow_credentials=True,
     allow_methods=["*"],
